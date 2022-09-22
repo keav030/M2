e412abc26e580adf519b11b3122c5a0d0ec4cf55
Kevin <kevinesteban2010@hotmail.com>
2022-09-22 00:26:31 -0500

    homework 3

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -7,9 +7,15 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
-
+  if(matchFunc(startEl)){
+    resultSet.push(startEl)
+  }
   // TU CÓDIGO AQUÍ
-  
+  for (let i = 0; i < startEl.children.length; i++) {
+    let elements = traverseDomAndCollectElements(matchFunc, startEl.children[i])
+    resultSet = [...resultSet, ...elements]
+  }
+  return resultSet
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +24,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0] === "#")return "id"
+  if(selector[0] === ".")return "class"
+  if(selector.split(".").length > 1 )return "tag.class"
+  if(selector)return "tag"
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,16 +39,30 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+    matchFunction = function(element) {
+      return "#"+element.id === selector
+    }
   } else if (selectorType === "class") {
-    
+    matchFunction = function(el) {
+      for (let index = 0; index < el.classList.length; index++) {
+        if("."+ el.classList[index] === selector) return true
+      }
+      return false
+    }
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = function(element) {
+      let [tag, clase] = selector.split(".")
+      return matchFunctionMaker(tag)(element) && matchFunctionMaker("."+clase)(element)
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction = function(element) {
+      return element.tagName.toLowerCase() === selector
+    }
   }
   return matchFunction;
 };
+
+
 
 var $ = function(selector) {
   var elements;
